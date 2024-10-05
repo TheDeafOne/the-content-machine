3cd28311e5d0ad439dbab6487d8486aa7b28a0e6
TheDeafOne <Woodburnkb20@gcc.edu>
2024-10-05 16:29:07 -0400

    init commit

---
+++ src/__init__.py
@@ -0,0 +1,38 @@
+import os
+import random
+import string
+import urllib.request
+
+import requests
+from flask import Flask
+from flask_login import LoginManager
+
+import src.db as database
+
+scriptdir = os.path.dirname(os.path.abspath(__file__))
+dbfile = os.path.join(scriptdir, "db.sqlite3")
+
+def _setup_db(app: Flask):
+    with app.app_context():
+        database.db.init_app(app)
+        if (os.getenv("RELOAD_DB") == "True"):
+            print("Reloading DB...\n")
+            database.db.drop_all()
+            database.db.create_all()
+            print("DB Reloaded\n")
+        else:
+            database.db.create_all()
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+    app.config['SECRET_KEY'] = 'NotDefault'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{dbfile}"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    _setup_db(app)
+
+    return app
